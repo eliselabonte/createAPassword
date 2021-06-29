da16a39697c43cbebe1411e1b893ecf818192760
Elise_LaBonte <emlabonte714@gmail.com>
2021-06-29 12:11:10 -0400

    css changes, functionality of js

--- script.js
+++ script.js
@@ -1,83 +1,94 @@
-// // Assignment Code
-// let generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   let password = generatePassword();
-//   let passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword(finalPassword) {
 
-//   passwordText.value = password;
+  var password = finalPassword;
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  
+}
 
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", defaultAndCaptureData);
 
 
-
-
-
-
-// 1. define variables
-
-//    
-// 2. define function
-// 3. 
-
-
-
-// define variables
-
-// define lengthRange as value of range scale
-const lengthRange = document.getElementById('lengthRange');
-// define lengthNum as value of number input
-const lengthNum = document.getElementById('lengthNum');
 // define form 
 const form = document.getElementById('passwordGeneratorForm')
+
 // define form variables
 const uppercase = document.getElementById('uppercase');
 const numbers = document.getElementById('numbers');
 const specChar = document.getElementById('specChar');
+const length = document.getElementById('lengthRange')
 
 // define range of uppercase, numbers, and specChar using character codes
+const uppercaseCodes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseCodes = "abcdefghijklmnopqrstuvwxyz";
+const numberCodes    = "1234567890";
+const specCharCodes  = "!@#$%^&*<>?|~";
+let allCodes         = "abcdefghijklmnopqrstuvwxyz";
 
 
+// create function
+function defaultAndCaptureData(event) {
+  
+  allCodes = "abcdefghijklmnopqrstuvwxyz";
 
-// adding event listeners for change in Range/Num
-lengthRange.addEventListener('input', syncLength);
-lengthNum.addEventListener('input', syncLength);
+  event.preventDefault;
 
-// add event listener to prevent default reload of page on submit
-form.addEventListener('submit', 
-  e=>{e.preventDefault()}
-  const password = generatePassword(includeUppercase, includeNumbers, includeSpecChar, charLength),
-  const includeUppercase = uppercase.checked,
-  const includeNumbers = numbers.checked,
-  const includespecChar = specChar.checked,
-  )
+  const wantsUppercase = uppercase.checked;
+  const wantsNumbers = numbers.checked;
+  const wantsSpecChar = specChar.checked;
+  console.log(wantsUppercase)
 
-function generatePassword(includeUppercase, includeNumbers, includeSpecChar, charLength)  {
-
-}
-
-function arrayCalc(low, high) {
-  const array = [],
-  for 
-}
-
-
-// create function syncLength
-function syncLength(e) {
-  const value = e.target.value,
-    lengthRange.value = value,
-    lengthNum.value = value,
-}
-
-// 
-function submit() {
+  if (wantsUppercase) {
+    allCodes += uppercaseCodes;
+  }; 
+  if (wantsNumbers) {
+    allCodes += numberCodes;
+  };
+  if (wantsSpecChar) {
+    allCodes += specCharCodes;
+  };
+  generatePassword();
 
 }
+
+
+// create randomNumber within the range of the length of index
+function generatePassword() {
+
+  let pass = '';
+  
+  for (i=1;i <= length.value; i++) {
+    
+    let character = Math.floor(Math.random() * (allCodes.length+1)); 
+  
+    pass += allCodes.charAt(character);
+  };
+
+ return writePassword(pass);
+
+};
+
+
+// use randomNumber to 
+
+
+// define function that happens when form is submitted
+
+
+//  function generate(){
+//   const includeUppercase = uppercase.checked;
+//   const includeNumbers = numbers.checked;
+//   const includeSpecChar = specChar.checked;
+//   const charLength = length.value;
+//   writePassword(); //includeUppercase, includeNumbers, includeSpecChar, charLength
+// };
+
+// writePassword();
+
 
